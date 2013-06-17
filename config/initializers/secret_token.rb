1f07b50d64ee34790e955afc483468b488f1b159
jhon F <jfrodrig@misena.edu.co>
2013-06-17 16:00:49 -0500

    My First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ejempligit::Application.config.secret_key_base = '86ead8ce65e64de55d7a54a378bec025721caa8e8f8ffbd20bedc88e198bf1fcbbf74e80517ba90fcbc6f594226c7ce6252db407d0430dcdbfc161adc2310d5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ejempligit::Application.config.session_store :cookie_store, key: '_ejempligit_session'
